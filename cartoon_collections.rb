8e908a95cbab9c28d4b77a1e7009c6823334a441
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-03 21:43:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.collect { |e| e = e.uperCase + "!" }
+  array.collect { |e| e = e.upcase + "!" }
 end
 
 def long_planeteer_calls# code an argument here
